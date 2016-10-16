fdfd84ded8966bbd6b2d0a1467f7beec316f3f32
SakataGintokiKagura <523192490@qq.com>
2016-10-16 14:36:09 +0800

    添加地形创造信息类

--- Scripts/Model/TerrainProxy.cs
+++ Scripts/Model/TerrainProxy.cs
@@ -3,6 +3,16 @@ using System.Collections;
 using PureMVC.Patterns;
 using System.Collections.Generic;
 
+public class TerrainCreateInfor
+{
+    public ITerrain terrain;
+    public Dictionary<float, GoldEnum> coin;
+    public TerrainCreateInfor(ITerrain terrain, Dictionary<float, GoldEnum> coin)
+    {
+        this.terrain = terrain;
+        this.coin = coin;
+    }
+}
 public class TerrainProxy : Proxy {
 
     public new const string NAME = "terrainProxy";
@@ -14,6 +24,7 @@ public class TerrainProxy : Proxy {
     public void OnCreateTerrain(TerrainEnum terrain,Dictionary<float,GoldEnum> coin)
     {
         ITerrain newTerrain = new OneTerrain();
-        SendNotification(EventsEnum.terrainCreateSuccess,newTerrain);
+        TerrainCreateInfor terrainCreateInfor = new TerrainCreateInfor(newTerrain, coin);
+        SendNotification(EventsEnum.terrainCreateSuccess,terrainCreateInfor);
     }
 }
