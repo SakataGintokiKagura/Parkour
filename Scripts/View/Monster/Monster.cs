af795218f3c02c4e7851700cc2a29b4c982ccb84
Ryaness <786151061@qq.com>
2016-10-21 13:01:36 +0800

    注释

--- Scripts/View/Monster/Monster.cs
+++ Scripts/View/Monster/Monster.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// 怪物的AI控制   张子庆
+/// </summary>
 public class Monster : MonoBehaviour
 {
     private IMonsterMediator monsterMediator;
@@ -20,24 +23,28 @@ public class Monster : MonoBehaviour
     void Update()
     {
        
-        //如果怪物出现在视野之中
+        
         foreach (KeyValuePair<IBlology, GameObject> kv in monster)
         {
+            //判断怪物是否进入屏幕视野之中 以及进入视野之后的动画控制
             if (Camera.main.WorldToViewportPoint(kv.Value.transform.position).x > 0 &&
                 Camera.main.WorldToViewportPoint(kv.Value.transform.position).x < 1)
             {
                 kv.Value.GetComponent<Animator>().SetBool("InView", true);
+                //判断玩家是否进入怪物的攻击范围，如果进入攻击范围对玩家进行攻击
                 if (kv.Value.transform.position.x - player.transform.position.x < kv.Key.normalAttackDistance &&
                     kv.Value.transform.position.x - player.transform.position.x > -1)
                 {
                     kv.Value.GetComponent<Animator>().SetBool("Attack", true);
                     kv.Value.GetComponent<Animator>().SetBool("InView", false);
+                    //瓦斯弹的AI
                     if (kv.Value.name == "Monster_Wasi(Clone)")
                     {
                         kv.Value.transform.FindChild("Monster_Wasi@skin").gameObject.SetActive(false);
                         kv.Value.transform.FindChild("Detonator-Simple").gameObject.GetComponent<Detonator>().enabled = true;
                         kv.Value.name = "Monster_Wasi";
                     }
+                    //龙的AI
                     if (kv.Value.name == "Monster_Dragon(Clone)")
                     {
                         (GameObject.Instantiate(sphere, kv.Value.transform.FindChild("sphere"), false) as GameObject).transform.position = kv.Value.transform.FindChild("sphere").position;
@@ -47,12 +54,13 @@ public class Monster : MonoBehaviour
                 }
 
             }
+            //对进入屏幕视野的玩家离开屏幕的时候销毁
             else if (Camera.main.WorldToViewportPoint(kv.Value.transform.position).x < 0)
             {
                 outList.Add(kv.Value);
             }
         }
-
+        //对进入屏幕视野的玩家离开屏幕的时候销毁
         foreach (var temp in outList)
         {
             monsterMediator.OnDestroyMonster(temp);
