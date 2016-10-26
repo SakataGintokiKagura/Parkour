2d4623dc7bf956190f01d8c31fe82aaaa6f42b90
Ryaness <786151061@qq.com>
2016-10-26 22:39:38 +0800

    添加内存管理

---
+++ Scripts/MemoryController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MemoryController
+{
+    private static MemoryController _instance;
+    public List<GameObject> monsterContainerList=new List<GameObject>();
+
+    public static MemoryController instance
+    {
+        get
+        {
+            if (_instance==null)
+            {
+                _instance=new MemoryController();
+            }
+
+            return _instance;
+        }
+    }
+
+}
